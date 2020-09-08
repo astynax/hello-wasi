f6f94850057a2353ddc2cd286b7c4e53ad474551
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-09-08 16:57:15 +0300

    add some CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-use ansi_term::Colour::{Blue, Yellow};
+use ansi_term::{Colour};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum MsgColour {
+    Blue,
+    Yellow,
+}
+
+fn parse_color(src: &str) -> Result<MsgColour, String> {
+    if src == "blue" {
+        Result::Ok(MsgColour::Blue)
+    } else if src == "yellow" {
+        Result::Ok(MsgColour::Yellow)
+    } else {
+        Result::Err("Bad color!".to_string())
+    }
+}
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(
+        short, long,
+        parse(try_from_str = parse_color),
+        default_value = "blue",
+    )]
+    colour: MsgColour
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
+    let colour = match opt.colour {
+        MsgColour::Blue => Colour::Blue,
+        MsgColour::Yellow => Colour::Yellow,
+    };
+
     println!(
         "{}, {}!",
-        Blue.bold().paint("Hello"),
-        Yellow.underline().paint("World")
+        Colour::Blue.bold().paint("Hello"),
+        colour.underline().paint("World")
     );
 }
